d0275adcc6c8842bf40d0e3a1513c7f6dd88cb5d
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2025-03-10 23:44:44 +0200

    Start process instance programmatically

--- Process Payment/src/main/java/org/paymentprocess/PaymentApplication.java
+++ Process Payment/src/main/java/org/paymentprocess/PaymentApplication.java
@@ -6,6 +6,8 @@ import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.paymentprocess.Constant.ZEEBE_ADDRESS;
 import static org.paymentprocess.Constant.ZEEBE_AUTHORIZATION_SERVER_URL;
@@ -28,6 +30,19 @@ public class PaymentApplication {
                 .gatewayAddress(ZEEBE_ADDRESS)
                 .build()) {
 
+            // Define variables to pass to process through the job worker
+            final Map<String, Object> variables = new HashMap<>();
+            variables.put("amount", 100);
+
+            // Creates and starts an instance of the process
+            client.newCreateInstanceCommand()
+                    .bpmnProcessId("paymentProcess")
+                    .latestVersion()
+                    .variables(variables)
+                    .send()
+                    .join();
+
+            // Register a job worker for the "processPayment" job type
             final JobWorker paymentWorker = client.newWorker()
                     .jobType("processPayment")
                     .handler(new PaymentHandler())
