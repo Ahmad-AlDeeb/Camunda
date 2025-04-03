0fb2343eda01ccabd8c8448009883a7a80b2de20
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2025-04-04 01:36:47 +0200

    Add Hiring Process demo

---
+++ hiring-process/src/main/java/com/deeb/hiring_process/HiringProcessApplication.java
@@ -0,0 +1,13 @@
+package com.deeb.hiring_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringProcessApplication.class, args);
+	}
+
+}
