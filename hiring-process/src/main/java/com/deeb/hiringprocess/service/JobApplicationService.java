3df021e00fec7bd5acc7ecc7a58a65e1031d463d
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2025-04-05 22:04:06 +0200

    Update calculateCvScore to be async and to be done properly in its time

--- hiring-process/src/main/java/com/deeb/hiringprocess/service/JobApplicationService.java
+++ hiring-process/src/main/java/com/deeb/hiringprocess/service/JobApplicationService.java
@@ -5,7 +5,9 @@ import com.deeb.hiringprocess.entity.JobApplication;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import static com.deeb.hiringprocess.Constant.CamundaConstant.PROCESS_DEFINITION_ID;
 import static com.deeb.hiringprocess.Constant.CamundaConstant.ZEEBE_TOKEN;
@@ -13,35 +15,38 @@ import static java.lang.String.format;
 
 @Service
 public class JobApplicationService {
-    private final ZeebeClientService zeebeClientService;
-
-    public JobApplicationService(ZeebeClientService zeebeClientService) {
-        this.zeebeClientService = zeebeClientService;
-    }
+    private static final ZeebeClientService zeebeClientService = new ZeebeClientService();
 
     public void create(JobApplication jobApplication) {
-        Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("processDefinitionId", PROCESS_DEFINITION_ID);
-        requestBody.put("variables", jobApplication);
-
+        Map<String, Object> requestBody = Map.of(
+                "processDefinitionId", PROCESS_DEFINITION_ID,
+                "variables", jobApplication
+        );
         zeebeClientService.startProcessInstance(ZEEBE_TOKEN, requestBody);
-        calculateCvScore();
     }
 
-    public void calculateCvScore() {
-        Map<String, Object> activateRequestBody = new HashMap<>();
-        activateRequestBody.put("type", "CalculateCvScore");
-        activateRequestBody.put("maxJobsToActivate", 1);
-        activateRequestBody.put("timeout", 10000);
+    public static void calculateCvScore() throws Exception {
+        Map<String, Object> activateRequestBody = Map.of(
+                "type", "CalculateCvScore",
+                "timeout", 10000,
+                "maxJobsToActivate", 1
+        );
+        while (true) {
+            List<Job> jobs = zeebeClientService.activateJobs(ZEEBE_TOKEN, activateRequestBody).jobs();
 
-        Job activatedJob = zeebeClientService.activateJobs(ZEEBE_TOKEN, activateRequestBody).jobs().getFirst();
-        Long jobKey = activatedJob.jobKey();
-        String name = (String) activatedJob.variables().get("name");
+            if (jobs.isEmpty()) {
+                Thread.sleep(1000);
+            } else for (Job job : jobs) {
+                Long jobKey = job.jobKey();
+                String name = (String) job.variables().get("name");
 
-        System.out.println(format("Calculating %s's CV score...", name));
+                System.out.println(format("Calculating %s's CV score...", name));
 
-        Map<String, Object> completeRequestBody = new HashMap<>();
-        completeRequestBody.put("variables", Map.of("score", 90));
-        zeebeClientService.completeJob(ZEEBE_TOKEN, jobKey, completeRequestBody);
+                Map<String, Object> completeRequestBody = Map.of("variables", Map.of("score", 95));
+                zeebeClientService.completeJob(ZEEBE_TOKEN, jobKey, completeRequestBody);
+            }
+        }
     }
+
+
 }
