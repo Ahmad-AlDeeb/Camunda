f3bba0ee2d5a8d3dedf5805498585a3da52b17f1
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2025-06-01 15:15:49 +0300

    Enable auto deployment

--- Camunda-7/hiring-process-7/src/main/java/com/deeb/hiringprocess/HiringProcess.java
+++ Camunda-7/hiring-process-7/src/main/java/com/deeb/hiringprocess/HiringProcess.java
@@ -1,11 +1,9 @@
 package com.deeb.hiringprocess;
 
-import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableProcessApplication
 public class HiringProcess {
   public static void main(String... args) {
     SpringApplication.run(HiringProcess.class, args);
