702b60fc8ad4afb31c1d3cc255b05c0cf6d76fc9
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2025-06-02 19:50:51 +0300

    Add timer event to remind applicant to respond

---
+++ Camunda-7/hiring-process-7/src/main/java/com/deeb/hiringprocess/service/SendReminderDelegate.java
@@ -0,0 +1,23 @@
+package com.deeb.hiringprocess.service;
+
+import com.deeb.hiringprocess.sendgrid.EmailDispatcher;
+import jakarta.inject.Named;
+import lombok.AllArgsConstructor;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import static com.deeb.hiringprocess.constant.HiringConstant.TO_EMAIL;
+
+@Named
+@AllArgsConstructor
+public class SendReminderDelegate implements JavaDelegate {
+    private final EmailDispatcher emailDispatcher;
+    private static final String SUBJECT = "Reminder to respond to your job application";
+    // write this email body in a way that it is very short
+    private static final String BODY = "Dear Applicant, this is a gentle reminder to respond to your job application.";
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        emailDispatcher.dispatchEmail(TO_EMAIL, SUBJECT, BODY);
+    }
+}
